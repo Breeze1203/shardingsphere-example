e13701afd83de3eaa116f61a49ed4fc05ed8d3ac
pengtao <tpeng9240@gmail.com>
2025-08-17 17:39:04 +0800

    数据脱敏

---
+++ mask-example/src/main/java/org/apache/shardingsphere/example/mask/ExampleMain.java
@@ -0,0 +1,20 @@
+package org.apache.shardingsphere.example.mask;
+
+import org.apache.shardingsphere.example.mask.service.ExampleService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import java.sql.SQLException;
+
+@EntityScan(basePackages = "org.apache.shardingsphere.example.mask.entity")
+@SpringBootApplication
+public class ExampleMain {
+    
+    public static void main(final String[] args) throws SQLException {
+        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(ExampleMain.class, args)) {
+            ExampleService exampleService = applicationContext.getBean(ExampleService.class);
+            exampleService.run();
+        }
+    }
+}
